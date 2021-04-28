67d126ed5e163111a507cd05b04719b30a2832fa
Maitê Caetano <maitecaetano@MacBook-Air-de-Maite.local>
2021-04-28 15:21:05 -0300

    adding data and image folder

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const personagens = [
+  {
+    id: 1,
+    name: 'Naruto Uzumaki',
+    image: 'images/naruto.png'
+  },
+  {
+    id: 2,
+    name: 'Super Shock',
+    image: 'images/super-choque.jpeg'
+  },
+  {
+    id: 3,
+    name: 'Merida',
+    image: 'images/valente.jpg'
+  },
+  {
+    id: 4,
+    name: 'Goku',
+    image: 'images/goku.jpeg'
+  },
+  {
+    id: 5,
+    name: 'Homer Simpson',
+    image: 'images/homer.png'
+  },
+  {
+    id: 6,
+    name: 'Flash',
+    image: 'images/flash.png'
+  },
+  {
+    id: 7,
+    name: 'Bob Esponja',
+    image: 'images/bob-esponja.jpeg'
+  }
+];
+
+export default personagens;
